52d3bc2449b8dc39d25984f5e679ed3af6c16ccc
Steven Nunez <octosteve@github.com>
2023-10-12 13:42:44 -0400

    Show cloned contents

--- bin/run.sh
+++ bin/run.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
+set -x
 env
 pwd
 apt update && apt install -y git-core
 git clone https://$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git $GITHUB_WORKSPACE/
 
-echo "Loading contents of $RUNNER_WORKSPACE"
-ls -l $RUNNER_WORKSPACE
-
 echo "Loading contents of $GITHUB_WORKSPACE"
-ls -l $GITHUB_WORKSPACE
-
-echo "Loading Action directory"
-ls -l $GITHUB_ACTION_PATH
-
-echo "Loading current directory"
-ls -l .
+ls -la 
+cat README
